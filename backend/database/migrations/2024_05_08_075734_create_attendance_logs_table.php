a39aa884e955e4caf46fe8070ec50a23f61cc783
Daneris Mendoza <106788394+DanerisMendoza@users.noreply.github.com>
2024-05-09 12:01:16 +0800

    daneris part complete

--- backend/database/migrations/2024_05_08_075734_create_attendance_logs_table.php
+++ backend/database/migrations/2024_05_08_075734_create_attendance_logs_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('attendance_logs', function (Blueprint $table) {
             $table->id();
             $table->string('hashed_user_id');
-            $table->dateTime('date_time');
-            $table->string('status');
             $table->timestamps();
         });
     }
---
+++ frontend/src/layouts/default/Default.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <AppBar />
+    <SideNav/>
+    <View />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import AppBar from './AppBar.vue'
+  import View from './View.vue'
+  import SideNav from './SideNav.vue'
+</script>
