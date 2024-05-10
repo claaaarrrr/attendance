ad8958938c0b3107284a77379bfd1d1d7aff42b1
dyiroooo <20200097m.ibia.jiroallen.bscs@gmail.com>
2024-05-10 17:26:51 +0800

    [Attendance Logs]
    Attendance are filtered now in the user's side.

--- backend/app/Http/Controllers/AttendanceLogController.php
+++ backend/app/Http/Controllers/AttendanceLogController.php
@@ -7,43 +7,45 @@ use Illuminate\Http\Request;
 use App\Models\AttendanceLog;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AttendanceLogController extends Controller
 {
     public function getAttendance(Request $request)
     {
-        $user_role = Auth::user()->user_role;
-        $hashid = Auth::user()->hashed_user_id;
+        $user = Auth::user();
+        $user_role = $user->user_role;
+        $hashid = $user->hashed_user_id;
 
+        \Log::info($user_role);
         $itemsPerPage = $request->get('itemsPerPage', 10);
         $page = $request->get('page', 1);
 
-        if ($user_role == 0) {
-            $attResults = User::select(
-                DB::raw("CONCAT_WS(' ', users.first_name, users.middle_name, users.last_name, users.suffix) AS name"),
-                'users.gender',
-                'users.email',
-                DB::raw("TIME(attendance_logs.created_at) as time_in"),
-            )
-                ->leftJoin('attendance_logs', 'users.hashed_user_id', '=', 'attendance_logs.hashed_user_id')
-                ->whereNotNull('attendance_logs.created_at')
-                ->get();
-            $totalItems = $attResults->count();
+        $query = User::select(
+            DB::raw("CONCAT_WS(' ', users.first_name, users.middle_name, users.last_name, users.suffix) AS name"),
+            'users.gender',
+            'users.email',
+            DB::raw("TIME(attendance_logs.created_at) as time_in")
+        )->leftJoin('attendance_logs', 'users.hashed_user_id', '=', 'attendance_logs.hashed_user_id')
+            ->whereNotNull('attendance_logs.created_at');
 
-            if ($totalItems < 0) {
-                return response()->json([
-                    'message' => 'No Results Found.'
-                ], 204);
-            }
-
-            return response()->json([
-                'data' => array_values($attResults->forPage($page, $itemsPerPage)->toArray()),
-                'total' => $totalItems,
-                'per_page' => $itemsPerPage,
-                'current_page' => $page,
-            ]);
-        } else {
-            return;
+        if ($user_role == 1) {
+            $query->where('users.hashed_user_id', $hashid);
         }
+
+        $attResults = $query->paginate($itemsPerPage, ['*'], 'page', $page);
+
+        if ($attResults->isEmpty()) {
+            return response()->json([
+                'message' => 'No Results Found.'
+            ], 204);
+        }
+
+        return response()->json([
+            'data' => $attResults->items(),
+            'total' => $attResults->total(),
+            'per_page' => $itemsPerPage,
+            'current_page' => $page,
+        ]);
     }
 }
